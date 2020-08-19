3cf594fcc91ed0c41861853ebf44621087d98c8a
Desenv0 <gti.desenvolvimento@uss.br>
2020-08-19 13:19:52 -0300

    correçao endereçamento

--- datastore/store.go
+++ datastore/store.go
@@ -10,18 +10,19 @@ import (
 // Config struct
 type Config struct {
 	Server struct {
-		Host string `yaml:"host"`
+		Host       string   `yaml:"host"`
+		Equipments []string `yaml:"equipments"`
 	} `yaml:"server"`
 }
 
 // ReadConfigFile read configuration file
 func ReadConfigFile() (cfg *Config) {
-	f, err := os.Open("config.yml")
+	f, err := os.Open("/home/desenv0/Projetos/go/src/slam-device-emulator/config.yml")
 	utils.CheckError(err)
 	defer f.Close()
 
 	decoder := yaml.NewDecoder(f)
-	err = decoder.Decode(cfg)
+	err = decoder.Decode(&cfg)
 	utils.CheckError(err)
 
 	return cfg
